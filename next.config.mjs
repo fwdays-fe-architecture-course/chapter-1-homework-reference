4b48d446e540e29436c46c50a8a08093f9d87f92
akhudiak <next.teacher@lectrum.io>
2024-08-07 22:31:30 +0100

    lesson-1-reference

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_SUPABASE_URL: "https://buorxogcjfanwgabzhbo.supabase.co",
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1b3J4b2djamZhbndnYWJ6aGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI5Mzc4MDEsImV4cCI6MjAzODUxMzgwMX0.XSsrK7ZvmNqrOBI9lp2sxD-saR5yv7F3u4FjXUHCA7I"
+    }
+};
+
+export default nextConfig;
