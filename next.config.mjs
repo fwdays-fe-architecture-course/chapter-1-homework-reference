6089f75adba70843e8836dd1ada54ea321a8b82f
akhudiak <next.teacher@lectrum.io>
2024-08-13 15:54:38 +0100

    adjusted supabase creds

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_SUPABASE_URL: "https://buorxogcjfanwgabzhbo.supabase.co",
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1b3J4b2djamZhbndnYWJ6aGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI5Mzc4MDEsImV4cCI6MjAzODUxMzgwMX0.XSsrK7ZvmNqrOBI9lp2sxD-saR5yv7F3u4FjXUHCA7I"
+        NEXT_PUBLIC_SUPABASE_URL: "https://niyiilltqlsmzbtwzeaf.supabase.co",
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5peWlpbGx0cWxzbXpidHd6ZWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM1NTk5NTksImV4cCI6MjAzOTEzNTk1OX0.Op9ppEcbNGt2UGEfz16Qlx5ydRk7arc3ucaawqPIsQM"
     }
 };
 
