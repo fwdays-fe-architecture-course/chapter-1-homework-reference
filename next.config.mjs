a4c09610f95f3c88ec791f79f76edbf5802c1cb6
akhudiakov97 <akhudiakov97@gmail.com>
2024-12-15 08:47:37 +0000

    added next url

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_SUPABASE_URL: "https://niyiilltqlsmzbtwzeaf.supabase.co",
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5peWlpbGx0cWxzbXpidHd6ZWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM1NTk5NTksImV4cCI6MjAzOTEzNTk1OX0.Op9ppEcbNGt2UGEfz16Qlx5ydRk7arc3ucaawqPIsQM"
+        NEXT_PUBLIC_SUPABASE_URL: "https://sujfwlyknottidatkizb.supabase.co",
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1amZ3bHlrbm90dGlkYXRraXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2NTkzMzIsImV4cCI6MjA0NDIzNTMzMn0.a2iBv45z1D-IwkbnUliqSnlCqx7a0SJanANcwfVzQPk"
     }
 };
 
